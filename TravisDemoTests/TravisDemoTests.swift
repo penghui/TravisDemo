f8822dda957cab494f7f06b73fe606d6d171b524
penghui <penghui@gmail.com>
2016-01-29 09:46:40 +0800

    deleted UITest target

--- TravisDemoTests/TravisDemoTests.swift
+++ TravisDemoTests/TravisDemoTests.swift
@@ -24,12 +24,14 @@ class TravisDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        XCTAssert(false)
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+            sleep(1)
         }
     }
     
